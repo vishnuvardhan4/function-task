c8c7545a91b24327ed9ea3822f9af34be5cfd6ab
vishnuvardhan4 <vishnuvardhanyadav4@gmail.com>
2023-03-18 12:51:17 +0530

    uploading remove add task files

---
+++ addremove.js
@@ -0,0 +1,15 @@
+
+const subjects=["Eglish","Telugu","Hindi","Social","Maths","Science"];
+var a=document.getElementById("sub").value;
+
+console.log(subjects[1])
+
+function addfun(){
+    a.push();
+    console.log(subjects)
+}
+addfun()
+
+// var annony = function(){
+//     console.log("Hi welocme")
+// }
